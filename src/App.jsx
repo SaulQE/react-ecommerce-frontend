56c34aeb59eec0640c3190317e4d4dd3988d8a90
SaulQE <saul.esquivel.028@gmail.com>
2024-07-11 00:07:44 -0500

    add sidebar

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,22 @@
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { Sidebar } from './components/dashboard/Sidebar';
+import { ProductList } from './pages/ProductList';
+
 function App() {
 	return (
-		<>
-			<div></div>
-		</>
+		<Router>
+			<div className='App flex'>
+				<Sidebar />
+				<div className='p-4 sm:ml-64 w-full'>
+					<div className='p-4 border-2 border-gray-200 rounded-lg dark:border-gray-700'>
+						<Routes>
+							<Route path='/products' element={<ProductList />} />
+							{/* Otras rutas */}
+						</Routes>
+					</div>
+				</div>
+			</div>
+		</Router>
 	);
 }
 
