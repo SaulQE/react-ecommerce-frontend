49f7b0bcfec5880d9e7f55633dd8c4f89a63ce78
SaulQE <saul.esquivel.028@gmail.com>
2024-07-12 21:02:57 -0500

    add style glass tailwind

--- src/components/table/TablePrueba.jsx
+++ src/components/table/TablePrueba.jsx
@@ -3,40 +3,68 @@ import PropTypes from 'prop-types';
 export const TablePrueba = ({ columns, data }) => {
 	return (
 		<>
-			<div className='flex min-h-screen items-center justify-center bg-white'>
-				<div className='p-6 overflow-scroll px-0'>
-					<table className='w-full min-w-max table-auto text-left'>
-						<thead>
+			<div className='p-4'>
+				<div className='bg-white/50 rounded-lg shadow-md overflow-auto'>
+					<table className='w-full'>
+						<thead className='border-b-2 border-gray-500'>
 							<tr>
-								{columns.map(column => (
-									<th
-										key={column.accessor}
-										className='border-y border-blue-gray-100 bg-blue-gray-50/50 p-4'
-									>
-										<p className='block antialiased font-sans text-sm text-blue-gray-900 font-normal leading-none opacity-70'>
-											{column.Header}
-										</p>
-									</th>
-								))}
+								<th className='p-3 text-sm font-bold tracking-wide text-left'>
+									Name
+								</th>
+								<th className='p-3 text-sm font-bold tracking-wide text-left'>
+									LastName
+								</th>
+								<th className='p-3 text-sm font-bold tracking-wide text-left'>
+									Email
+								</th>
+								<th className='p-3 text-sm font-bold tracking-wide text-left'>
+									Avenida
+								</th>
 							</tr>
 						</thead>
-						<tbody>
-							{data.map((row, rowIndex) => (
-								<tr key={rowIndex} className='hover'>
-									{columns.map(column => (
-										<td
-											key={column.accessor}
-											className='p-4 border-b border-blue-gray-50'
-										>
-											<div className='flex items-center gap-3'>
-												<p className='block antialiased font-sans text-sm leading-normal text-blue-gray-900 font-bold'>
-													{row[column.accessor]}
-												</p>
-											</div>
-										</td>
-									))}
-								</tr>
-							))}
+						<tbody className='divide-y divide-white'>
+							<tr className='bg-white/50 even:bg-slate-50 text-white'>
+								<td className='p-3 text-sm text-gray-700 whitespace-no-wrap'>
+									Albert Robert
+								</td>
+								<td className='p-3 text-sm text-gray-700 whitespace-no-wrap'>
+									Bravo Quintana
+								</td>
+								<td className='p-3 text-sm text-gray-700 whitespace-no-wrap'>
+									Bravo gmail.com
+								</td>
+								<td className='p-3 text-sm text-gray-700 whitespace-no-wrap'>
+									Av. TuCasa
+								</td>
+							</tr>
+							<tr className='bg-white/50 even:bg-white/35'>
+								<td className='p-3 text-sm text-gray-700 whitespace-no-wrap'>
+									Albert Robert
+								</td>
+								<td className='p-3 text-sm text-gray-700 whitespace-no-wrap'>
+									Bravo Quintana
+								</td>
+								<td className='p-3 text-sm text-gray-700 whitespace-no-wrap'>
+									Bravo gmail.com
+								</td>
+								<td className='p-3 text-sm text-gray-700 whitespace-no-wrap'>
+									Av. TuCasa
+								</td>
+							</tr>
+							<tr className='bg-white/50 even:bg-slate-50'>
+								<td className='p-3 text-sm text-gray-700 whitespace-no-wrap'>
+									Albert Robert
+								</td>
+								<td className='p-3 text-sm text-gray-700 whitespace-no-wrap'>
+									Bravo Quintana
+								</td>
+								<td className='p-3 text-sm text-gray-700 whitespace-no-wrap'>
+									Bravo gmail.com
+								</td>
+								<td className='p-3 text-sm text-gray-700 whitespace-no-wrap'>
+									Av. TuCasa
+								</td>
+							</tr>
 						</tbody>
 					</table>
 				</div>
