9f6a8299d6e4dd825852d944095ef79535f7f8ac
SaulQE <saul.esquivel.028@gmail.com>
2024-07-13 23:02:52 -0500

    add colors defect

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,7 +6,12 @@ export default {
 		'./node_modules/flowbite/**/*.js',
 	],
 	theme: {
-		extend: {},
+		extend: {
+			colors: {
+				'glass-dark': '#0F090C',
+				'glass-light': '#FFEDE0',
+			},
+		},
 	},
 	// eslint-disable-next-line no-undef
 	plugins: [require('daisyui'), require('flowbite/plugin')],
