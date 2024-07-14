8a22de528234579d61eab206e71595d39da99c2d
SaulQE <saul.esquivel.028@gmail.com>
2024-07-13 23:03:07 -0500

    add sidebar part one

--- src/App.jsx
+++ src/App.jsx
@@ -5,17 +5,12 @@ import { ProductList } from './pages/ProductList';
 function App() {
 	return (
 		<Router>
-			<div className='App flex'>
-				<Sidebar />
-				<div className='p-4 sm:ml-64 w-full'>
-					<div className='p-4 border-2 border-gray-200 rounded-lg dark:border-gray-700'>
-						<Routes>
-							<Route path='/products' element={<ProductList />} />
-							{/* Otras rutas */}
-						</Routes>
-					</div>
-				</div>
-			</div>
+			<Sidebar />
+
+			<Routes>
+				{/* <Route path='/products' element={<ProductList />} /> */}
+				{/* Otras rutas */}
+			</Routes>
 		</Router>
 	);
 }
