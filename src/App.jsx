323907f6cd003597d273aea5f206868976a8c669
SaulQE <saul.esquivel.028@gmail.com>
2024-07-23 21:29:36 -0500

    movil sidebar

--- src/App.jsx
+++ src/App.jsx
@@ -5,12 +5,15 @@ import { ProductList } from './pages/ProductList';
 function App() {
 	return (
 		<Router>
-			<Sidebar />
-
-			<Routes>
-				<Route path='/products' element={<ProductList />} />
-				{/* Otras rutas */}
-			</Routes>
+			<div className='flex h-screen'>
+				<Sidebar />
+				<div className='flex-1 p-4 overflow-auto'>
+					<Routes>
+						<Route path='/products' element={<ProductList />} />
+						{/* Otras rutas */}
+					</Routes>
+				</div>
+			</div>
 		</Router>
 	);
 }
