ac5422084ae9ec61da9dbfbc2f49a0e9034e7b06
SaulQE <saul.esquivel.028@gmail.com>
2024-07-30 16:01:27 -0500

    add Link create

--- src/pages/ProductList.jsx
+++ src/pages/ProductList.jsx
@@ -1,6 +1,7 @@
 import { useState, useEffect } from 'react';
 import axios from 'axios';
 import DataTable from '../components/table/DataTable';
+import { Link } from 'react-router-dom';
 
 export const ProductList = () => {
 	const [products, setProducts] = useState([]);
@@ -24,9 +25,12 @@ export const ProductList = () => {
 	return (
 		<>
 			<h1 className='text-2xl font-bold mb-4'>Product List</h1>
+			<Link to='/products/create'>
+				<button className='bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded mb-4'>
+					Create New Product
+				</button>
+			</Link>
 			<DataTable columns={columns} data={products} />
 		</>
 	);
 };
-
-export default ProductList;
