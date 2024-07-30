11139e0887a2189a7f5801bf19df5d29e85cd220
SaulQE <saul.esquivel.028@gmail.com>
2024-07-30 16:00:20 -0500

    add route product create

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import { Sidebar } from './components/dashboard/Sidebar';
 import { ProductList } from './pages/ProductList';
+import { CreateProduct } from './pages/CreateProduct';
 
 function App() {
 	return (
@@ -10,6 +11,7 @@ function App() {
 				<div className='flex-1 p-4 overflow-auto'>
 					<Routes>
 						<Route path='/products' element={<ProductList />} />
+						<Route path='/products/create' element={<CreateProduct />} /> {/* Ruta anidada */}
 						{/* Otras rutas */}
 					</Routes>
 				</div>
