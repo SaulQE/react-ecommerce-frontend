0051818526bbfd6cdab79db810b12f6beb35bf2e
SaulQE <saul.esquivel.028@gmail.com>
2024-07-30 16:00:29 -0500

    add EntityForm

---
+++ src/components/forms/EntityForm.jsx
@@ -0,0 +1,38 @@
+import PropTypes from 'prop-types';
+import { FormButton } from './FormButton';
+import { FormInput } from './FormInput';
+
+export const EntityForm = ({
+	formData,
+	handleChange,
+	handleSubmit,
+	fields,
+}) => {
+	return (
+		<form
+			className='bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4'
+			onSubmit={handleSubmit}
+		>
+			{fields.map((field, index) => (
+				<FormInput
+					key={index}
+					label={field.label}
+					type={field.type}
+					name={field.name}
+					value={formData[field.name]}
+					onChange={handleChange}
+					placeholder={field.placeholder}
+					required={field.required}
+				/>
+			))}
+			<FormButton label='Submit' type='submit' />
+		</form>
+	);
+};
+
+EntityForm.propTypes = {
+	formData: PropTypes.object.isRequired,
+	handleChange: PropTypes.func.isRequired,
+	handleSubmit: PropTypes.func.isRequired,
+	fields: PropTypes.array.isRequired,
+};
