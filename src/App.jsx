68b93462f931d002c266472cd0b330eff354d313
SaulQE <saul.esquivel.028@gmail.com>
2024-08-01 19:20:30 -0500

    add Routes update and create

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,8 @@
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import { Sidebar } from './components/dashboard/Sidebar';
-import { ProductList } from './pages/ProductList';
-import { CreateProduct } from './pages/CreateProduct';
+import { ProductList } from './pages/products/ProductList';
+import { CreateProduct } from './pages/products/CreateProduct';
+import { UpdateProduct } from './pages/products/UpdateProduct';
 
 function App() {
 	return (
@@ -11,7 +12,11 @@ function App() {
 				<div className='flex-1 p-4 overflow-auto'>
 					<Routes>
 						<Route path='/products' element={<ProductList />} />
-						<Route path='/products/create' element={<CreateProduct />} /> {/* Ruta anidada */}
+						<Route path='/products/create' element={<CreateProduct />} />
+						<Route
+							path='/products/update/:productId'
+							element={<UpdateProduct />}
+						/>
 						{/* Otras rutas */}
 					</Routes>
 				</div>
